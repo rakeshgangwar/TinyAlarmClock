0a54fdae8e74c6509b6e7da1f1279cc0396aadef
Rakesh Gangwar <mail@rakeshgangwar.com>
2016-08-30 03:46:47 +0530

    Added Timepicker and Alarm icon

--- app/src/main/java/com/rakeshgangwar/tinyalarmclock/MainActivity.java
+++ app/src/main/java/com/rakeshgangwar/tinyalarmclock/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rakeshgangwar.tinyalarmclock;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +24,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        calendar=Calendar.getInstance();
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new TimePickerDialog(MainActivity.this,
+                            t,
+                            calendar.get(Calendar.HOUR_OF_DAY),
+                            calendar.get(Calendar.MINUTE),
+                            false).show();
+                }
+            });
+        }
     }
+
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            Toast.makeText(getApplicationContext(), ""+hourOfDay+" "+minute, Toast.LENGTH_SHORT).show();
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
