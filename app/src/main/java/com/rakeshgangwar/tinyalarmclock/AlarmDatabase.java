c9222eead726a692e5dd9ea5510345d74d70c927
Rakesh Gangwar <mail@rakeshgangwar.com>
2016-08-30 05:13:41 +0530

    ListView adapter and listener added + DB integration

---
+++ app/src/main/java/com/rakeshgangwar/tinyalarmclock/AlarmDatabase.java
@@ -0,0 +1,79 @@
+package com.rakeshgangwar.tinyalarmclock;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlarmDatabase extends SQLiteOpenHelper {
+
+    static AlarmDatabase instance = null;
+    static SQLiteDatabase database = null;
+
+    static final String databaseName = "AlarmDB";
+    static final int databaseVersion = 1;
+
+    public static final String tableAlarm = "alarm";
+    public static final String columnAlarmTime = "alarm_time";
+
+    public static void init(Context context){
+        if(null == instance){
+            instance=new AlarmDatabase(context);
+        }
+    }
+
+    public static SQLiteDatabase getDatabase() {
+        if(database == null) {
+            database=instance.getWritableDatabase();
+        }
+        return database;
+    }
+
+    public static long create(String time){
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(columnAlarmTime, time);
+
+        return getDatabase().insert(tableAlarm, null, contentValues);
+    }
+
+    public static int deleteAlarm(String time){
+        return getDatabase().delete(tableAlarm, columnAlarmTime+"=\""+time+"\"",null);
+    }
+
+    public static Cursor getCursor() {
+        String[] columns=new String[]{columnAlarmTime};
+        return getDatabase().query(tableAlarm,columns,null, null, null, null,
+                null);
+    }
+
+    AlarmDatabase(Context context) { super(context,databaseName,null,databaseVersion); }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + tableAlarm + " ( "
+                + columnAlarmTime + " TEXT primary key NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + tableAlarm);
+        onCreate(db);
+    }
+
+    public static List<String> getAll() {
+        List<String> alarms=new ArrayList<>();
+        Cursor cursor=AlarmDatabase.getCursor();
+        if(cursor.moveToFirst()) {
+            do{
+                String alarm=cursor.getString(0);
+                alarms.add(alarm);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return alarms;
+    }
+}
--- app/src/main/java/com/rakeshgangwar/tinyalarmclock/MainActivity.java
+++ app/src/main/java/com/rakeshgangwar/tinyalarmclock/MainActivity.java
@@ -3,26 +3,33 @@ package com.rakeshgangwar.tinyalarmclock;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Calendar calendar;
+    ListView listView;
+    private List<String> alarms=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        listView=(ListView)findViewById(R.id.listView);
         setSupportActionBar(toolbar);
         calendar=Calendar.getInstance();
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -38,13 +45,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+        AlarmDatabase.init(this);
+        fillListView();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value=(String)parent.getItemAtPosition(position);
+                AlarmDatabase.deleteAlarm(value);
+                fillListView();
+            }
+        });
     }
 
     TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             Toast.makeText(getApplicationContext(), ""+hourOfDay+" "+minute, Toast.LENGTH_SHORT).show();
-
+            AlarmDatabase.init(getApplicationContext());
+            AlarmDatabase.create(""+hourOfDay+" "+minute);
+            fillListView();
         }
     };
 
@@ -68,5 +87,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void fillListView(){
+        alarms=AlarmDatabase.getAll();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, alarms);
+        listView.setAdapter(adapter);
     }
 }
